383b745fff833570877494a3e1c587caf24dfc41
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2017-05-22 18:10:28 -0600

    file uploading complete

--- max/backend_routes/FileUpload.py
+++ max/backend_routes/FileUpload.py
@@ -1,8 +1,8 @@
 from flask_restful import Resource, reqparse
 from flask import request
 from werkzeug.utils import secure_filename
-from os.path import join
-from os.path import join
+from os.path import join, exists
+import os
 
 
 class FileUpload(Resource):
@@ -13,8 +13,17 @@ class FileUpload(Resource):
         return 'file uploaded successfully'
 
     def post(self):
-        image_file = request.files['imageInput']
+        output_directory = '/Users/sheshank.kodam/Desktop/dev/personal_projects/avatars'
+
+        if not exists(output_directory):
+            os.makedirs(output_directory)
+
+        image_file = request.files['avatar']
+        # first_name = request.files['firstName']
+        # last_name = request.files['lastName']
+        # dob = request.files['dateOfBirth']
+
         image_file_name = secure_filename(image_file.filename)
-        image_file.save(join('/Users/sheshank.kodam/Desktop/dev/personal_projects/Max', image_file_name))
+        image_file.save(join(output_directory, image_file_name))
         return 'file uploaded successfully1'
 
--- max/frontend_static/javascript/max.js
+++ max/frontend_static/javascript/max.js
@@ -12,8 +12,14 @@ $(document).ready(function () {
     $("#uploaded_file").change(function(){
         console.log("File upload received");
         previewImage(this);
-        saveImage(this);
+
     });
+
+    $(".btn_submit").click(function(){
+        saveImage();
+    });
+
+
 
     function previewImage(input) {
         console.log("Previewing image");
@@ -26,10 +32,20 @@ $(document).ready(function () {
         }
     }
 
-    function saveImage(input) {
+    function saveImage() {
         console.log("Saving image");
+        var oFiles = document.getElementById("uploaded_file").files;
+        var avatar = oFiles[0];
+        var firstName = document.getElementsByName("first_name")[0].value;
+        var lastName = document.getElementsByName("last_name")[0].value;
+        var dateOfBirth = document.getElementsByName("dob")[0].value;
+
         var frm = new FormData();
-        frm.append('imageInput', input.files[0]);
+        frm.append('avatar', avatar);
+        // frm.append('firstName', firstName);
+        // frm.append('lastName', lastName);
+        // frm.append('dateOfBirth', dateOfBirth);
+        console.log("Saving image");
         $.ajax({
             type: 'POST',
             url: '/upload',
